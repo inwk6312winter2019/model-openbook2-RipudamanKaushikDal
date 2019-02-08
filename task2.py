c1cda4fd1d1c62146065757a2eb67f6f90465f95
RipudamanKaushikDal <Ripudaman.kaushik@dal.ca>
2019-02-07 21:23:12 -0400

    reduced no. of arguements and added sub-task b and c

--- task2.py
+++ task2.py
@@ -1,6 +1,7 @@
 
+
 #task a
-def fun_1(index2,index1,string2,string1):
+def fun_1(string2,string1):
 	streets=[]
 	roads=[]
 	file1=open('Street_Centrelines.csv')
@@ -9,7 +10,7 @@ def fun_1(index2,index1,string2,string1):
 	for sentens in file2:
 		sent=sentens.strip()
 		list2=sent.split(',')
-		if list2[index2]==string2:
+		if list2[7]==string2:
 			if list2[9] not in streets:
 				streets.append(list2[9])
 			else:
@@ -18,8 +19,21 @@ def fun_1(index2,index1,string2,string1):
 	for lines in file1:
 		line=lines.strip()
 		list1=line.split(',')
-		if list1[index1]==string1:
+		if list1[10]==string1:
 			if list1[23] in streets:
 				roads.append(list1[4])
 	return roads
 print(fun_1(7,10,'Accessible','ARTERIAL'))
+
+#task b
+def fun2():
+	'''Prints local street with a  non standard bus stop'''
+	other=fun_1('Non-Standard','LOCAL STREET')
+	print(other)
+fun2()
+
+#task3
+def fun3():
+	new=fun_1('Inaccessible','MINOR COLLECTOR')
+	print(new)
+fun3()
