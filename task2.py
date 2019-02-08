1e5ba13f5d32c6dce086346397b1c06aabe01f7d
RipudamanKaushikDal <Ripudaman.kaushik@dal.ca>
2019-02-07 21:03:28 -0400

    Generalised the function for reuse

--- task2.py
+++ task2.py
@@ -1,6 +1,6 @@
 
 #task a
-def fun_1():
+def fun_1(index2,index1,string2,string1):
 	streets=[]
 	roads=[]
 	file1=open('Street_Centrelines.csv')
@@ -9,7 +9,7 @@ def fun_1():
 	for sentens in file2:
 		sent=sentens.strip()
 		list2=sent.split(',')
-		if list2[7]=='Accessible':
+		if list2[index2]==string2:
 			if list2[9] not in streets:
 				streets.append(list2[9])
 			else:
@@ -18,8 +18,8 @@ def fun_1():
 	for lines in file1:
 		line=lines.strip()
 		list1=line.split(',')
-		if list1[10]=='ARTERIAL':
+		if list1[index1]==string1:
 			if list1[23] in streets:
 				roads.append(list1[4])
 	return roads
-print(fun_1())
+print(fun_1(7,10,'Accessible','ARTERIAL'))
